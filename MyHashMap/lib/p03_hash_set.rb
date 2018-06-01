85f0e511b543c51fb69b12dfd5f0f9748514d28d
Daniel Moisoff (and Derick Castillo) <Dmoisoff@gmail.com (and 9derick@gmail.com)>
2018-06-01 16:55:39 -0400

    Complete P03_HashSet

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -7,12 +7,26 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      @count += 1
+      if count > num_buckets
+        resize!
+      end
+      @store.shift
+      @store.push([key]) 
+    end
   end
 
   def include?(key)
+    @store.include?([key])
   end
 
   def remove(key)
+    if include?(key)
+      @count -= 1
+      @store.delete([key])
+      @store.unshift([])
+    end
   end
 
   private
@@ -26,5 +40,7 @@ class HashSet
   end
 
   def resize!
+    empty_buckets = Array.new(num_buckets) {[]}
+    @store = empty_buckets += @store
   end
 end
